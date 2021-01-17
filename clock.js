b6a4549cb769c3247b605d7ea71b63edb8a70c8f
Simon Brand <simon.brand@postadigitale.de>
2021-01-17 00:01:10 +0000

    Use sans-serif as font

--- clock.js
+++ clock.js
@@ -95,7 +95,7 @@ function Clock() {
 			ctx.stroke();
 
 			ctx.fillStyle = 'white';
-			ctx.font = '40px serif';
+			ctx.font = '40px sans-serif';
 			ctx.textAlign = 'center';
 			ctx.fillText(result['text'], xCenter + 30, yCenter - radius + 15);
 
